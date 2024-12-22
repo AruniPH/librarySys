8ce99ad55c51176f5e9877b21725d668b79ad023
AruniPH <aruniprasangikanmd@gmail.com>
2024-12-22 22:46:45 +0530

    Book Create, API Completed.

--- src/main/java/com/l/libsys/repo/BookRepository.java
+++ src/main/java/com/l/libsys/repo/BookRepository.java
@@ -4,6 +4,9 @@ import com.l.libsys.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book , Integer> {
+    Optional <Book> findByIsbn(String isbn);
 }
